ab0b480260d7270037fb709cf3210a0df4b3aa9a
Geoffrey Ko <kogw@uci.edu>
2017-10-23 17:27:07 -0700

    add step() skeleton

--- src/MyAI.java
+++ src/MyAI.java
@@ -18,9 +18,18 @@
 // ======================================================================
 
 import java.util.ArrayList;
+import java.util.Stack;
+
+
+/* map: stores probabilities for each square;
+ *      empty = 1, hazardous = 0
+ */
 
 public class MyAI extends Agent
 {
+  	private static final double UNEXPLORED = -1;
+  	private static final double EMPTY = 1;
+  
   	private enum Direction
     {
   		NORTH,
@@ -30,16 +39,21 @@ public class MyAI extends Agent
     }
   
   	private Direction direction;
+  	private Point currentPoint;
   	private ArrayList<ArrayList<Double>> map;
+  	private Stack<Point> path;
   
 	public MyAI ( )
 	{
 		// ======================================================================
 		// YOUR CODE BEGINS
 		// ======================================================================
-        direction = Direction.EAST;
+      	direction = Direction.EAST;
+      	currentPoint = new Point();		// initialized to (1, 1)
       	ArrayList<ArrayList<Double>> map = new ArrayList<>();
 		map.add(new ArrayList<Double>());
+      
+      	path = new Stack<>();
 		// ======================================================================
 		// YOUR CODE ENDS
 		// ======================================================================
@@ -57,8 +71,24 @@ public class MyAI extends Agent
 		// ======================================================================
 		// YOUR CODE BEGINS
 		// ======================================================================
+        if (stench) {
+
+        }
+        if (breeze) {
+
+        }
+        if (glitter) {
+
+        }
+        if (bump) {
+
+        }
+        if (scream) {
+
+        }
 		
-		return Action.CLIMB;
+        step(Direction.EAST);
+		return Action.FORWARD;
 		// ======================================================================
 		// YOUR CODE ENDS
 		// ======================================================================
@@ -67,9 +97,54 @@ public class MyAI extends Agent
 	// ======================================================================
 	// YOUR CODE BEGINS
 	// ======================================================================
-
+    private void initializeDirectionMap() {
+      	map = new ArrayList<>();
+      	map.add(new ArrayList<Double>());
+    }
+  
+    private int getUpperBound() {
+      	return map.get(0).size();
+    }
+  	
+    private int getRightBound() {
+          return map.size();
+    }
+  
+    private void step(Direction direction) {
+        switch (direction) {
+          	case NORTH:
+          		// if you move up, append to current ArrayList
+          		if ((currentPoint.getY()+1) >= getUpperBound()) {
+          			map.get(currentPoint.getY()).add(UNEXPLORED);
+                }
+          		currentPoint.addY(1);
+          		break;
+          		
+          	case SOUTH:
+          		currentPoint.addY(-1);
+          		break;
+          		
+          	case EAST:
+          		// append a new ArrayList to the outer array
+          		if (currentPoint.getX()+1 >= getRightBound())
+                {
+                  	map.add(new ArrayList<Double>());
+                  	for (int i = 0; i < getUpperBound(); ++i)
+                    	map.get(currentPoint.getX()+1).add(UNEXPLORED);
+                }
+          		currentPoint.addX(1);
+          		break;
+          		
+          	case WEST:
+          		currentPoint.addX(-1);
+          		break;
+        }
+      	map.get(currentPoint.getX()).set(currentPoint.getY(), EMPTY);
+      	// save the (safe) point you just moved onto
+      	path.push(currentPoint);
+    }
 
 	// ======================================================================
 	// YOUR CODE ENDS
 	// ======================================================================
-}
+}    
